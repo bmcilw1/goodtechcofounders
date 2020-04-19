28749de67c1e59596b967e127320dc98611c0cd5
Brian McIlwain <brian.mcilwian@technicallyemployed.com>
2020-04-18 23:25:15 -0700

    Changed date format

--- src/components/ArticlesRoll.js
+++ src/components/ArticlesRoll.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql, StaticQuery } from 'gatsby'
-import PreviewCompatibleImage from './PreviewCompatibleImage'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql, StaticQuery } from "gatsby";
+import PreviewCompatibleImage from "./PreviewCompatibleImage";
 
 class ArticlesRoll extends React.Component {
   render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+    const { data } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
 
     return (
       <div className="columns is-multiline">
@@ -15,7 +15,7 @@ class ArticlesRoll extends React.Component {
             <div className="is-parent column is-6" key={post.id}>
               <article
                 className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost ? 'is-featured' : ''
+                  post.frontmatter.featuredpost ? "is-featured" : ""
                 }`}
               >
                 <header>
@@ -36,8 +36,8 @@ class ArticlesRoll extends React.Component {
                     >
                       {post.frontmatter.title}
                     </Link>
-                    <span> &bull; </span>
-                    <span className="subtitle is-size-5 is-block">
+                    <br />
+                    <span className="subtitle is-size-5">
                       {post.frontmatter.date}
                     </span>
                   </p>
@@ -54,7 +54,7 @@ class ArticlesRoll extends React.Component {
             </div>
           ))}
       </div>
-    )
+    );
   }
 }
 
@@ -64,7 +64,7 @@ ArticlesRoll.propTypes = {
       edges: PropTypes.array,
     }),
   }),
-}
+};
 
 export default () => (
   <StaticQuery
@@ -101,4 +101,4 @@ export default () => (
     `}
     render={(data, count) => <ArticlesRoll data={data} count={count} />}
   />
-)
+);
