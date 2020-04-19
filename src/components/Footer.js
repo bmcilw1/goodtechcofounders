e2886749ebc12a166faf753a6afa4d5fc56752b4
Brian McIlwain <brian.mcilwian@technicallyemployed.com>
2020-04-18 17:14:05 -0700

    Changed blog to articles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,8 @@ import { Link } from "gatsby";
 
 import logo from "../img/logo.svg";
 import facebook from "../img/social/facebook.svg";
-import instagram from "../img/social/instagram.svg";
 import twitter from "../img/social/twitter.svg";
-import vimeo from "../img/social/vimeo.svg";
+import github from "../img/github-icon.svg";
 
 const Footer = class extends React.Component {
   render() {
@@ -35,11 +34,6 @@ const Footer = class extends React.Component {
                       </Link>
                     </li>
                     <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
                       <a
                         className="navbar-item"
                         href="/admin/"
@@ -56,8 +50,8 @@ const Footer = class extends React.Component {
                 <section>
                   <ul className="menu-list">
                     <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
+                      <Link className="navbar-item" to="/articles">
+                        Articles
                       </Link>
                     </li>
                     <li>
@@ -69,32 +63,29 @@ const Footer = class extends React.Component {
                 </section>
               </div>
               <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
+                <a
+                  href="https://github.com/bmcilw1/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={github}
+                    alt="Github"
+                    style={{ width: "1em", height: "1em" }}
+                  />
+                </a>
+                <a title="facebook" href="https://facebook.com" target="_blank">
                   <img
                     src={facebook}
                     alt="Facebook"
                     style={{ width: "1em", height: "1em" }}
                   />
                 </a>
-                <a title="twitter" href="https://twitter.com">
+                <a title="twitter" href="https://twitter.com" target="_blank">
                   <img
                     className="fas fa-lg"
                     src={twitter}
                     alt="Twitter"
-                    style={{ width: "1em", height: "1em" }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: "1em", height: "1em" }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
                     style={{ width: "1em", height: "1em" }}
                   />
                 </a>
