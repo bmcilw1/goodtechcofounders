4b8768a533095f18c3277789c9569c33b11c35ce
Brian McIlwain <brian.mcilwian@technicallyemployed.com>
2020-04-20 11:13:43 -0700

    Fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,15 +9,15 @@ const Footer = class extends React.Component {
   render() {
     return (
       <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
+        <div className="has-text-centered">
           <img
             src={logo}
             alt="Kaldi"
             style={{ width: "14em", height: "10em" }}
           />
         </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
+        <div className="has-text-centered">
+          <div className="container">
             <div className="columns">
               <div className="column">
                 <section className="menu">
