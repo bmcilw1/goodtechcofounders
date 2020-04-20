06c04cda6dd0fdd028ae082eaaea4bd2c9ed2b8e
Brian McIlwain <brian.mcilwian@technicallyemployed.com>
2020-04-20 16:48:46 -0700

    Added image to about page

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -1,19 +1,24 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { AboutPageTemplate } from '../../templates/about-page'
+import React from "react";
+import PropTypes from "prop-types";
+import { AboutPageTemplate } from "../../templates/about-page";
 
-const AboutPagePreview = ({ entry, widgetFor }) => (
+const AboutPagePreview = ({ entry, widgetFor, getAsset }) => (
   <AboutPageTemplate
-    title={entry.getIn(['data', 'title'])}
-    content={widgetFor('body')}
+    title={entry.getIn(["data", "title"])}
+    content={widgetFor("body")}
+    aboutImage={{
+      image: getAsset(entry.getIn(["data", "aboutImage", "image"])),
+      alt: entry.getIn(["data", "aboutImage", "alt"]),
+    }}
   />
-)
+);
 
 AboutPagePreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
   widgetFor: PropTypes.func,
-}
+  getAsset: PropTypes.func,
+};
 
-export default AboutPagePreview
+export default AboutPagePreview;
--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -3,15 +3,27 @@ import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
+import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
 
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
+export const AboutPageTemplate = ({
+  title,
+  content,
+  contentComponent,
+  aboutImage,
+}) => {
   const PageContent = contentComponent || Content;
 
   return (
     <section className="section">
       <div className="container">
-        <h2 className="title">{title}</h2>
-        <PageContent className="content" content={content} />
+        <h2 className="title has-text-centered">{title}</h2>
+        <div className="columns">
+          <PreviewCompatibleImage
+            className="column is-one-third"
+            imageInfo={aboutImage}
+          />
+          <PageContent className="content column" content={content} />
+        </div>
       </div>
     </section>
   );
@@ -21,6 +33,7 @@ AboutPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string,
   contentComponent: PropTypes.func,
+  aboutImage: PropTypes.object.isRequired,
 };
 
 const AboutPage = ({ data }) => {
@@ -32,6 +45,7 @@ const AboutPage = ({ data }) => {
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
         content={post.html}
+        aboutImage={post.frontmatter.aboutImage}
       />
     </Layout>
   );
@@ -49,6 +63,16 @@ export const aboutPageQuery = graphql`
       html
       frontmatter {
         title
+        aboutImage {
+          image {
+            childImageSharp {
+              fluid(quality: 64) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          alt
+        }
       }
     }
   }
