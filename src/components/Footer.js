04f98217b7cffa9d64a43d98d93af3ff76604521
Brian McIlwain <brian.mcilwian@technicallyemployed.com>
2020-04-21 14:12:32 -0700

    Went back to old footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ const Footer = class extends React.Component {
       <footer className="footer has-background-black has-text-white-ter has-text-centered">
         <img src={logo} alt="Kaldi" style={{ width: "14em", height: "10em" }} />
         <div className="container">
-          <section className="columns menu-list">
+          <section className="columns is-multiline menu-list">
             {NavigationData.links
               .filter((data) => data.showInFooter)
               .map((data) => {
@@ -35,28 +35,32 @@ const Footer = class extends React.Component {
                   </Link>
                 );
               })}
-            <a
-              className="column is-narrow"
-              title="Github"
-              href="https://github.com/bmcilw1/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="icon">
-                <FaGithub />
-              </span>
-            </a>
-            <a
-              className="column is-narrow"
-              title="LinkedIn"
-              href="https://www.linkedin.com/in/brian-mcilwain/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="icon">
-                <FaLinkedin />
-              </span>
-            </a>
+            <div className="column">
+              <div className="columns">
+                <a
+                  className="column"
+                  title="Github"
+                  href="https://github.com/bmcilw1/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <span className="icon">
+                    <FaGithub />
+                  </span>
+                </a>
+                <a
+                  className="column"
+                  title="LinkedIn"
+                  href="https://www.linkedin.com/in/brian-mcilwain/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <span className="icon">
+                    <FaLinkedin />
+                  </span>
+                </a>
+              </div>
+            </div>
           </section>
         </div>
       </footer>
