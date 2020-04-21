e32e029e2f56a183881479eb64f670791e136584
Brian McIlwain <brian.mcilwian@technicallyemployed.com>
2020-04-21 15:35:45 -0700

    Simplified home page

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -63,40 +63,27 @@ export const IndexPageTemplate = ({
         </h3>
       </div>
     </div>
-    <section className="section section--gradient">
+
+    <section className="section">
       <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div className="content">
-                  <div className="tile">
-                    <h1 className="title">{mainpitch.title}</h1>
-                  </div>
-                  <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
-                  </div>
-                </div>
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      {heading}
-                    </h3>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  <ArticlesRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/articles">
-                      Read more
-                    </Link>
-                  </div>
-                </div>
-              </div>
+        <div className="content">
+          <div className="tile">
+            <h1 className="title">{mainpitch.title}</h1>
+          </div>
+          <div className="tile">
+            <h3 className="subtitle">{mainpitch.description}</h3>
+          </div>
+          <h3 className="has-text-weight-semibold is-size-2">{heading}</h3>
+          <p>{description}</p>
+          <div className="column is-12">
+            <h3 className="has-text-weight-semibold is-size-2">
+              Latest articles
+            </h3>
+            <ArticlesRoll />
+            <div className="has-text-centered">
+              <Link className="button" to="/articles">
+                Read more
+              </Link>
             </div>
           </div>
         </div>
