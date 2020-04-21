d5fcd82d617fc3f0aee660f806d037dd9adfec2d
Brian McIlwain <brian.mcilwian@technicallyemployed.com>
2020-04-21 13:14:45 -0700

    Column version of footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,34 +12,32 @@ const Footer = class extends React.Component {
       <footer className="footer has-background-black has-text-white-ter has-text-centered">
         <img src={logo} alt="Kaldi" style={{ width: "14em", height: "10em" }} />
         <div className="container">
-          <section className="tile menu-list is-ancestor">
-            <div className="tile is-parent is-10">
-              {NavigationData.links
-                .filter((data) => data.showInFooter)
-                .map((data) => {
-                  if (data.useExternalLink) {
-                    return (
-                      <a
-                        className="navbar-item tile is-child"
-                        href={data.path}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        {data.text}
-                      </a>
-                    );
-                  }
-
+          <section className="columns menu-list">
+            {NavigationData.links
+              .filter((data) => data.showInFooter)
+              .map((data) => {
+                if (data.useExternalLink) {
                   return (
-                    <Link className="navbar-item tile is-child" to={data.path}>
+                    <a
+                      className="navbar-item column"
+                      href={data.path}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
                       {data.text}
-                    </Link>
+                    </a>
                   );
-                })}
-            </div>
-            <div className="tile is-parent">
+                }
+
+                return (
+                  <Link className="navbar-item column" to={data.path}>
+                    {data.text}
+                  </Link>
+                );
+              })}
+            <div className="column columns">
               <a
-                className="tile is-child"
+                className="column"
                 title="Github"
                 href="https://github.com/bmcilw1/"
                 target="_blank"
@@ -50,7 +48,7 @@ const Footer = class extends React.Component {
                 </span>
               </a>
               <a
-                className="tile is-child"
+                className="column"
                 title="LinkedIn"
                 href="https://www.linkedin.com/in/brian-mcilwain/"
                 target="_blank"
