38982827e3f52427337b2844176acfb27842cda6
Brian McIlwain <brian.mcilwian@technicallyemployed.com>
2020-04-21 17:31:44 -0700

    Added articles to tags

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -2,17 +2,11 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import { Link, graphql } from "gatsby";
 import Layout from "../components/Layout";
+import { ArticleCard } from "../components/ArticlesRoll";
 
 class TagRoute extends React.Component {
   render() {
-    const posts = this.props.data.allMarkdownRemark.edges;
-    const postLinks = posts.map((post) => (
-      <li key={post.node.fields.slug}>
-        <Link key={post.node.fields.slug} to={post.node.fields.slug}>
-          <h2 className="is-size-2">{post.node.frontmatter.title}</h2>
-        </Link>
-      </li>
-    ));
+    const articles = this.props.data.allMarkdownRemark.edges;
     const tag = this.props.pageContext.tag;
     const title = this.props.data.site.siteMetadata.title;
     const totalCount = this.props.data.allMarkdownRemark.totalCount;
@@ -31,10 +25,18 @@ class TagRoute extends React.Component {
                 style={{ marginBottom: "6rem" }}
               >
                 <h3 className="title is-size-4 is-bold-light">{tagHeader}</h3>
-                <ul className="taglist">{postLinks}</ul>
-                <p>
-                  <Link to="/tags/">Browse all tags</Link>
-                </p>
+                <div className="columns">
+                  {articles.map((article) => (
+                    <ArticleCard
+                      className="column is-half"
+                      article={article.node}
+                      key={article.node.fields.slug}
+                    />
+                  ))}
+                </div>
+                <Link className="button is-link" to="/tags/">
+                  Browse all tags
+                </Link>
               </div>
             </div>
           </div>
