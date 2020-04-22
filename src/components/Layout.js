8277c579a0939473470c7b56ee86c4aa7c80d408
Brian McIlwain <brian.mcilwian@technicallyemployed.com>
2020-04-21 22:24:04 -0700

    Removed theme from header

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -38,7 +38,6 @@ const TemplateWrapper = ({ children }) => {
           href={`${withPrefix("/")}img/safari-pinned-tab.svg`}
           color="#ff4400"
         />
-        <meta name="theme-color" content="#fff" />
 
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
