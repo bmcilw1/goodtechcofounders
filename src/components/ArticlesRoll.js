0a03d760a54b22aee0b8fb4df55831774200e01f
Brian McIlwain <brian.mcilwian@technicallyemployed.com>
2020-04-21 17:18:14 -0700

    Factored out article card

--- src/components/ArticlesRoll.js
+++ src/components/ArticlesRoll.js
@@ -3,6 +3,68 @@ import PropTypes from "prop-types";
 import { Link, graphql, StaticQuery } from "gatsby";
 import PreviewCompatibleImage from "./PreviewCompatibleImage";
 
+class ArticleCard extends React.Component {
+  render() {
+    const { article } = this.props;
+
+    return (
+      <article
+        className={`tile is-child box notification ${
+          article.frontmatter.featuredpost ? "is-featured" : ""
+        }`}
+      >
+        <header className="columns">
+          {article.frontmatter.featuredimage ? (
+            <div className="column is-one-third">
+              <PreviewCompatibleImage
+                imageInfo={{
+                  image: article.frontmatter.featuredimage,
+                  alt: `featured image thumbnail for post ${article.frontmatter.title}`,
+                }}
+              />
+            </div>
+          ) : null}
+          <p className="column">
+            <Link
+              className="title has-text-primary is-size-4"
+              to={article.fields.slug}
+            >
+              {article.frontmatter.title}
+            </Link>
+            <br />
+            <span className="subtitle is-size-5">
+              {article.frontmatter.date}
+            </span>
+          </p>
+        </header>
+        <p>
+          {article.excerpt}
+          <br />
+          <br />
+          <Link className="button" to={article.fields.slug}>
+            Keep Reading →
+          </Link>
+        </p>
+      </article>
+    );
+  }
+}
+
+ArticleCard.propTypes = {
+  article: PropTypes.shape({
+    excerpt: PropTypes.string,
+    fields: PropTypes.shape({
+      slug: PropTypes.string,
+    }),
+    frontmatter: PropTypes.shape({
+      title: PropTypes.string,
+      date: PropTypes.string,
+      featuredpost: PropTypes.bool,
+      featuredimage: PropTypes.object,
+    }),
+  }).isRequired,
+};
+
 class ArticlesRoll extends React.Component {
   render() {
     const { data } = this.props;
@@ -13,44 +75,7 @@ class ArticlesRoll extends React.Component {
         {articles &&
           articles.map(({ node: article }) => (
             <div className="is-parent column is-6" key={article.id}>
-              <article
-                className={`tile is-child box notification ${
-                  article.frontmatter.featuredpost ? "is-featured" : ""
-                }`}
-              >
-                <header className="columns">
-                  {article.frontmatter.featuredimage ? (
-                    <div className="column is-one-third">
-                      <PreviewCompatibleImage
-                        imageInfo={{
-                          image: article.frontmatter.featuredimage,
-                          alt: `featured image thumbnail for post ${article.frontmatter.title}`,
-                        }}
-                      />
-                    </div>
-                  ) : null}
-                  <p className="column">
-                    <Link
-                      className="title has-text-primary is-size-4"
-                      to={article.fields.slug}
-                    >
-                      {article.frontmatter.title}
-                    </Link>
-                    <br />
-                    <span className="subtitle is-size-5">
-                      {article.frontmatter.date}
-                    </span>
-                  </p>
-                </header>
-                <p>
-                  {article.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button" to={article.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </article>
+              <ArticleCard article={article} />
             </div>
           ))}
       </div>
